d9f766bd09e440205456714c455bd1f62e75ce3d
AtteJokinen <atte.jokinen@tuni.fi>
2021-01-07 20:37:12 +0200

    Added tests

--- controllers/api/postNewEvent.js
+++ controllers/api/postNewEvent.js
@@ -2,9 +2,9 @@ const Event = require("../../models/event")
 const { generateId } = require("../../utils/index")
 
 const postNewEvent = async (request, response) => {
-  const { name, dates } = request.body
+  const { name, dates, id: idParam } = request.body
   const votes = dates.map((date) => ({ date, people: [] }))
-  const id = generateId()
+  const id = idParam || generateId()
   const event = new Event({
     id,
     name,
